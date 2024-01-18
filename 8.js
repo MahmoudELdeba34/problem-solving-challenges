70926fb319db961e67d6d0139105356f80bd55cc
Mahmoud Ahmed <64672610+MahmoudELdeba34@users.noreply.github.com>
2024-01-18 03:30:24 +0200

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,8 @@
+//Q8) Writeaprogramthat allows user to insert integer then print all numbers between 1 to  that’s number
+var num=parseInt(window.prompt("please enter any number :"));
+for(i=1;i<=num;i++){
+    
+    document.write(i+("</br>"));
+}
+document.write("prevouse all numbers between 1 to  that’s number ")
+
