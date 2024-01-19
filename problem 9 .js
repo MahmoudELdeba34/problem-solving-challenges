80fbad49297ad34e1388ce390868ec0afa1a4a98
Mahmoud Ahmed <64672610+MahmoudELdeba34@users.noreply.github.com>
2024-01-19 06:32:41 +0200

    Add files via upload

---
+++ problem 9 .js
@@ -0,0 +1,23 @@
+//Q7) Write a program that take character from user then if it is vowel chars (a,e,I,o,u) then print vowel otherwise print consonant
+
+var chars=window.prompt("please enter any character : ");
+if(chars=="a"){
+    document.write("vowel");
+}
+else if (chars=="e"){
+    document.write("vowel");
+}
+else if (chars=="i"){
+    document.write("vowel");
+}
+else if(chars=="o"){
+    document.write("vowel");
+}
+else if(chars=="u"){
+    document.write("vowel");
+}
+else{
+    document.write("consonant");
+}
+ 
+
