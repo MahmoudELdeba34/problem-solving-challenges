e39c3b8c480cafa0a70731bf3a55872d5b4bfa8a
Mahmoud Ahmed <64672610+MahmoudELdeba34@users.noreply.github.com>
2024-01-22 09:09:11 +0200

    Add files via upload

---
+++ 11.js
@@ -0,0 +1,5 @@
+//Q11) Write aprogram that take two integers then print the power
+
+var numOne=parseInt(window.prompt("please enter number one : "));
+var numTwo=parseInt(window.prompt("please enter number Two : "));
+document.write("the power is : " + numOne**numTwo);
