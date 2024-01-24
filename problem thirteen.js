9470750dfff7450d1c3f3ac4069ca0921bcabbef
Mahmoud Ahmed <64672610+MahmoudELdeba34@users.noreply.github.com>
2024-01-24 06:32:14 +0200

    Update and rename problem 13.js to problem thirteen.js

---
+++ problem thirteen.js
@@ -0,0 +1,65 @@
+//Q13) Write a program to input the month number and print the number of days in that month.
+//var numMonth=Number(window.prompt("please enter number of month :  ")); =============>golobal var
+
+//first solve
+if(numMonth==1){
+    document.write("number of days for this month is : 31 " );
+}
+else if(numMonth==2){
+    document.write("number of days for this month is : 28 " );
+}
+else if(numMonth==3){
+    document.write("number of days for this month is : 31 " );
+}
+else if (numMonth==4){
+    document.write("number of days for this month is : 30 " );
+}
+else if(numMonth==5){
+    document.write("number of days for this month is : 31 " );
+}
+else if (numMonth==6){
+    document.write("number of days for this month is : 30 " );
+}
+else if (numMonth==7){
+    document.write("number of days for this month is : 31 " );
+}
+else if(numMonth==8){
+    document.write("number of days for this month is : 30 " );
+}
+else if (numMonth==9){
+    document.write("number of days for this month is : 31 " );
+}
+else if(numMonth==10){
+    document.write("number of days for this month is : 30 " );
+}
+else if (numMonth==11){
+    document.write("number of days for this month is : 31 " );
+}
+else if(numMonth==12){
+    document.write("number of days for this month is : 31 " );
+}
+
+// second solve: 
+/*
+if(numMonth==2){
+    document.write("number of days for this month is : 28 " );
+}
+else if(numMonth==44){
+    document.write("number of days for this month is : 30 " );
+}
+else if (numMonth==6){
+    document.write("number of days for this month is : 30 " );
+}
+else if (numMonth==8){
+    document.write("number of days for this month is : 30 " );
+}
+else if (numMonth==10){
+    document.write("number of days for this month is : 30 " );
+}
+else {
+    document.write("number of days for this month is : 31 " );
+}
+*/
+//=======>end question thirteen <==========
+
+
