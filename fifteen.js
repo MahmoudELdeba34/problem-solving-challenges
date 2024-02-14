e7bf4a9023c1370eee43ce3062f6fcd4ba099732
Mahmoud Ahmed <64672610+MahmoudELdeba34@users.noreply.github.com>
2024-02-14 13:49:32 +0200

    Add files via upload

---
+++ fifteen.js
@@ -0,0 +1,25 @@
+// Q15) Write a program to print total number of days in month
+
+var numMonth=Number(window.prompt("please enter the number of month : "));
+switch(numMonth){
+    case 2:
+        document.write("the number of days of this month is : 28 ");
+        break;
+    case 4:
+        document.write("the number of days of this month is : 30 ");
+        break;
+    case 6 :
+        document.write("the number of days of this month is : 30 ");
+        break;
+    case 8 :
+        document.write("the number of days of this month is : 30 ");
+        break
+    case 10 :
+        document.write("the number of days of this month is : 30 ");
+        break;
+    default:
+        document.write("the number of days of this month is : 31 ");
+    
+}
+
+//=======>end question fifteen <==========
