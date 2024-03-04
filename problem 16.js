cb88439f25d03ff65915e58f8180c4bb4d3a99b6
Mahmoud Ahmed <64672610+MahmoudELdeba34@users.noreply.github.com>
2024-03-04 23:36:37 +0200

    Add files via upload

---
+++ problem 16.js
@@ -0,0 +1,22 @@
+//Q16) Write a program to check whether an alphabet is vowel or consonant
+
+var character=window.prompt("please enter any character");
+switch(character){
+    case "a" :
+        document.write("vowel");
+        break;
+    case "e" :
+        document.write("vowel");
+        break;
+    case "i" :
+        document.write("vowel");
+        break;
+    case "o" :
+        document.write("vowel");
+        break;
+    case "u" :
+        document.write("vowel");
+        break;
+    default:
+        document.write("consonant");
+}
