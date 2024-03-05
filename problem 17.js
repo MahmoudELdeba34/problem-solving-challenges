82e2a5094cf92b23c6e43ffe76001781ecba9562
Mahmoud Ahmed <64672610+MahmoudELdeba34@users.noreply.github.com>
2024-03-05 23:58:41 +0200

    Add files via upload

---
+++ problem 17.js
@@ -0,0 +1,14 @@
+//Q17) Write a program to find maximum between two numbers
+var numOne=Number(window.prompt("enter number one "));
+var numTwo=Number(window.prompt("enter number Two "));
+var numMax;
+switch(numMax){
+    case numMax:
+        if(numOne>numTwo){
+        document.write("number one is the max : " + numOne);
+        }
+        else{
+            document.write("number two is max : " + numTwo);
+        }
+        break;
+}
